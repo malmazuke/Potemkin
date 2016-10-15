1e3dc4908232e745f9a9bd02e57898b38ce25472
Mark Feaver <mark.feaver@gmail.com>
2016-10-15 14:30:39 +1300

    Added touch & mouse camera movement

---
+++ Assets/Scripts/Camera/MouseCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MouseCamera : MonoBehaviour {
+    
+    #region Public Properties
+    
+    public Camera mainCamera;
+    
+    #endregion
+
+    #region Private Properties
+    
+    Vector2 oldClickPosition;
+    
+    #endregion
+
+    #region Unity Methods
+
+    void Update () {
+        if (Input.GetMouseButtonDown (1)) {
+            oldClickPosition = Input.mousePosition;
+        } else if (Input.GetMouseButton (1)) {
+            Vector2 newClickPosition = Input.mousePosition;
+            mainCamera.transform.position += mainCamera.transform.TransformDirection ((Vector3)((oldClickPosition - newClickPosition) * mainCamera.orthographicSize / mainCamera.pixelHeight * 2f));
+            oldClickPosition = newClickPosition;
+        }
+    }
+    
+    #endregion
+}
