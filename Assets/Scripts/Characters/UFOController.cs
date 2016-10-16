3544c5347d78f693a45025d7dcf0abf79d91f131
Mark Feaver <mark.feaver@gmail.com>
2016-10-16 13:44:24 +1300

    Added some state to the UFO

--- Assets/Scripts/Characters/UFOController.cs
+++ Assets/Scripts/Characters/UFOController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum UFOPhase {
+public enum UFOState {
     Stopped,
     Moving,
     Observing
@@ -11,25 +11,28 @@ public class UFOController : MonoBehaviour {
 
     #region Public Properties
     
-    public UFOPhase currentPhase;
+    public UFOState currentState = UFOState.Stopped;
+    public UFOState nextState = UFOState.Moving;
     public float verticalFloatingMagnitide = 0.03f;
     public float verticalFloatingSpeed = 5.0f;
     public float movementSpeed = 1.0f;
     public float phaseLength = 1.0f;
     
     #endregion
+    
+    #region Private Properties
+    
+    bool isExecutingState = false;
+    
+    #endregion
 
 	#region Unity Methods
-    
-	void Awake () {
-	    currentPhase = UFOPhase.Stopped;
-	}
 	
 	void Update () {
         Oscillate ();
         
-	    if (currentPhase == UFOPhase.Stopped) {
-            StartCoroutine (Move ((int)phaseLength));
+	    if (!isExecutingState) {
+            StartCoroutine (PerformState (nextState, phaseLength));
         }
 	}
     
@@ -41,19 +44,37 @@ public class UFOController : MonoBehaviour {
         transform.localPosition = new Vector2 (transform.localPosition.x, Mathf.Sin (Time.time * verticalFloatingSpeed) * verticalFloatingMagnitide);
     }
     
-    IEnumerator Move (int lengthInSeconds) {
-        currentPhase = UFOPhase.Moving;
+    IEnumerator PerformState (UFOState phase, float lengthInSeconds) {
+        isExecutingState = true;
+        currentState = phase;
         float phaseRemaining = 0.0f;
-        float totalLength = (float)lengthInSeconds;
         
-        while (phaseRemaining < totalLength) {
-            transform.position = Vector2.MoveTowards (transform.position, (Vector2)transform.position + new Vector2 (1.0f, 0.0f), movementSpeed * Time.deltaTime);
+        while (phaseRemaining < lengthInSeconds) {
+            switch (currentState) {
+                case UFOState.Stopped:
+                    break;
+                case UFOState.Observing:
+                    Observe ();
+                    break;
+                case UFOState.Moving:
+                    Move ();
+                    break;
+            }
             
             phaseRemaining += Time.deltaTime;
             yield return null;
         }
         
-        currentPhase = UFOPhase.Stopped;
+        currentState = nextState;
+        isExecutingState = false;
+    }
+    
+    void Observe () {
+        // TODO: Observe what's around
+    }
+    
+    void Move () {
+        transform.position = Vector2.MoveTowards (transform.position, (Vector2)transform.position + new Vector2 (1.0f, 0.0f), movementSpeed * Time.deltaTime);
     }
     
     #endregion
