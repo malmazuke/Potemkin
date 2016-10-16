a3e4f5699d66703787c5108628669b09101d0f2e
Mark Feaver <mark.feaver@gmail.com>
2016-10-16 14:52:50 +1300

    Created some prefabs & started created a cursor to indicate where the UFO is offscreen

---
+++ Assets/Scripts/UFOTracker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOTracker : MonoBehaviour {
+    
+    #region Public Properties
+    
+    public UFOController ufo;
+    public GameObject cursorPrefab;
+    
+    #endregion
+    
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
